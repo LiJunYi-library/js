cb98398e76a113634f44f7ca7f20b1f5ab6a66e2
lijunyi <1040381245@qq.com>
2024-10-18 13:13:03 +0800

    RScrollVirtualFallsList ok

--- vue/v-view/components/scroll/virtual-falls-list/index.d.ts
+++ vue/v-view/components/scroll/virtual-falls-list/index.d.ts
@@ -1,3 +1,5 @@
 import { DefineComponent } from "vue";
 
 export declare const RScrollVirtualFallsList: DefineComponent<any>;
+export declare const RScrollVirtualFallsListV2: DefineComponent<any>;
+export declare const RScrollVirtualFallsListV1: DefineComponent<any>;

e3e6edd821ae34eade7c1552bfebf795f993ce9c
lijunyi <1040381245@qq.com>
2024-11-08 15:32:02 +0800

    树形结构 递归循环

--- rainbow-js/package/tree/index.js
+++ rainbow-js/package/tree/index.js
@@ -35,27 +35,26 @@ export function treeVagueFilter(
   }
 }
 
-
-
 /**
- * 树形结构 递归 循环
- * @params fun  每次循环的方法 item ，index ，list递归时的数组，parent 父node，roote 根node
- * @params keys 需要遍历的属性名
- * @params list 需要遍历 树
- * @params recursive 递归时的方法 list，parent，layer 层数，roote
- * @return 返回 list树
+ * 树形结构 递归循环
+ * @param {*} list 
+ * @param {*} fun 
+ * @param {*} recursive 
+ * @param {*} formatter 
+ * @param {*} layer 
+ * @param {*} parent 
+ * @returns 
  */
-export function recursiveForEach(list, formatter = fmt, fun, recursive, layer = -1, parent) {
+export function treeForEach(list = [], fun, recursive, formatter = fmt, layer = -1, parent) {
   let roote;
   roote = roote ? roote : list;
   layer++;
   if (recursive && recursive(list, parent, layer, roote)) return roote;
   for (let index = 0; index < list.length; index++) {
-    const element = list[index];
-    if (fun(element, index, list, parent, layer, roote)) return roote;
-    keys.forEach(key => {
-      if (element[key]) Array.recursiveForEach(fun, keys, [...element[key]], recursive, layer, element)
-    });
+    const child = list[index];
+    const children = formatter(child);
+    if (fun(child, index, list, parent, layer, roote)) return roote;
+    treeForEach(children, fun, recursive, formatter, layer, parent)
   }
   return roote;
 }

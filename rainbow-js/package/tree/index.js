f6afd6c31acee15eda3839d2ce183fdf2b40e01f
LiJunYi <39401717+LiJunYi-library@users.noreply.github.com>
2024-11-28 17:25:50 +0800

    树形结构 递归循环

--- rainbow-js/package/tree/index.js
+++ rainbow-js/package/tree/index.js
@@ -1,20 +1,20 @@
-import { arrayLoop, arrayRemove } from '../array';
-import { d } from './data'
+import { arrayLoop, arrayRemove } from "../array";
+import { d } from "./data";
 
 function fmt(item) {
-  return item?.children ?? []
+  return item?.children ?? [];
 }
 
 /**
  * 树形结构筛选 改变原对象
- * @param {*} children 
- * @param {*} formatter 
- * @param {*} formatterChildren 
+ * @param {*} children
+ * @param {*} formatter
+ * @param {*} formatterChildren
  * @param {*} isdeep
- * 
- * @param {*} copyC 
- * @param {*} parent 
- * @param {*} parentList 
+ *
+ * @param {*} copyC
+ * @param {*} parent
+ * @param {*} parentList
  */
 export function treeConditionRemove(
   children = [],
@@ -23,25 +23,33 @@ export function treeConditionRemove(
   formatterChildren = fmt,
   copyC = [...children],
   parent,
-  parentList) {
-  copyC.forEach(child => {
+  parentList,
+) {
+  copyC.forEach((child) => {
     const childList = formatterChildren(child);
-    if (!formatter(child) && !childList?.length) arrayRemove(children, child)
-    if (!isdeep && formatter(child)) return
-    if (childList?.length) treeConditionRemove(childList, formatter, isdeep, formatterChildren, [...(childList ?? [])], child, children)
+    if (!formatter(child) && !childList?.length) arrayRemove(children, child);
+    if (!isdeep && formatter(child)) return;
+    if (childList?.length)
+      treeConditionRemove(
+        childList,
+        formatter,
+        isdeep,
+        formatterChildren,
+        [...(childList ?? [])],
+        child,
+        children,
+      );
   });
 
   if (parent && parentList) {
     const pChildList = formatterChildren(parent);
-    if (!formatter(parent) && !pChildList?.length) arrayRemove(parentList, parent)
+    if (!formatter(parent) && !pChildList?.length) arrayRemove(parentList, parent);
   }
 }
 
 export function treeConditionDeepRemove(children = [], formatter, formatterChildren) {
-  treeConditionRemove(children, formatter, true, formatterChildren)
+  treeConditionRemove(children, formatter, true, formatterChildren);
 }
-
-
 
 function __treefilter(
   children = [],
@@ -55,34 +63,34 @@ function __treefilter(
   layer = -1,
 ) {
   layer++;
-  children.forEach(el => el.children = el.children?.map?.(val => ({ ...val })))
-  copyC.forEach(child => {
+  children.forEach((el) => (el.children = el.children?.map?.((val) => ({ ...val }))));
+  copyC.forEach((child) => {
     let childList = formatterChildren(child);
-    if (!formatter(child, layer, parent) && !childList?.length) arrayRemove(children, child)
-    if (!isdeep && formatter(child, layer, parent)) return
-    let copy = [...(childList ?? [])]
-    if (childList?.length) __treefilter(childList, formatter, isdeep, formatterChildren, copy, child, children, layer)
+    if (!formatter(child, layer, parent) && !childList?.length) arrayRemove(children, child);
+    if (!isdeep && formatter(child, layer, parent)) return;
+    let copy = [...(childList ?? [])];
+    if (childList?.length) __treefilter(childList, formatter, isdeep, formatterChildren, copy, child, children, layer);
   });
 
   if (parent && parentList) {
     const pChildList = formatterChildren(parent);
-    if (!formatter(parent, layer) && !pChildList?.length) arrayRemove(parentList, parent)
+    if (!formatter(parent, layer) && !pChildList?.length) arrayRemove(parentList, parent);
   }
 }
 /**
  * 树形结构筛选 不改变原对象 返回新树
- * @param {*} children 
- * @param {*} formatter 
+ * @param {*} children
+ * @param {*} formatter
  * @param {*} isdeep
- * @param {*} formatterChildren 
- * 
- * @param {*} copyC 
- * @param {*} parent 
- * @param {*} parentList 
+ * @param {*} formatterChildren
+ *
+ * @param {*} copyC
+ * @param {*} parent
+ * @param {*} parentList
  */
 export function treefilter(treeList = [], ...arg) {
   // const d =  treeList.map(el => ({ ...el }));
-  const d = JSON.parse(JSON.stringify(treeList))
+  const d = JSON.parse(JSON.stringify(treeList));
   __treefilter(d, ...arg);
   return d;
 }
@@ -91,13 +99,13 @@ export function treefilter(treeList = [], ...arg) {
 
 /**
  * 树形结构 递归循环
- * @param {*} list 
- * @param {*} fun 
- * @param {*} recursive 
- * @param {*} formatter 
- * @param {*} layer 
- * @param {*} parent 
- * @returns 
+ * @param {*} list
+ * @param {*} fun
+ * @param {*} recursive
+ * @param {*} formatter
+ * @param {*} layer
+ * @param {*} parent
+ * @returns
  */
 export function treeForEach(list = [], fun, recursive, formatter = fmt, layer = -1, parent) {
   let roote;
@@ -108,7 +116,7 @@ export function treeForEach(list = [], fun, recursive, formatter = fmt, layer = 
     const child = list[index];
     const children = formatter(child, index, parent, layer);
     if (fun(child, index, list, parent, layer, roote)) return roote;
-    treeForEach(children, fun, recursive, formatter, layer, parent)
+    if (children?.length) treeForEach(children, fun, recursive, formatter, layer, parent);
   }
   return roote;
 }
@@ -116,11 +124,6 @@ export function treeForEach(list = [], fun, recursive, formatter = fmt, layer = 
 export function treePathMap(tree = [], formatter, data = [], parent, path = [], layer = -1) {
   layer++;
   tree.forEach(() => {
-    path.push
-
-  })
-
+    path.push;
+  });
 }
-
-
-

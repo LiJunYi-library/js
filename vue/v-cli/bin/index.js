924b59f2754f5d09c9ff9ee7cb3f746f75cb10a0
LiJunYi <39401717+LiJunYi-library@users.noreply.github.com>
2024-12-30 16:14:34 +0800

    存

--- vue/v-cli/bin/index.js
+++ vue/v-cli/bin/index.js
@@ -3,7 +3,7 @@ const mGlobal = require("../global");
 mGlobal.init();
 const express = require("express");
 const webpack = require("webpack");
-const open = require('open');
+const open = require("open");
 const webpackDevMiddleware = require("webpack-dev-middleware");
 const webpackHotMiddleware = require("webpack-hot-middleware");
 const WebpackDevServer = require("webpack-dev-server");
@@ -15,84 +15,95 @@ const { configSetCss } = require("../loader/css");
 const { configSetImg } = require("../loader/img");
 const { configSetFont } = require("../loader/font");
 const { merge } = require("webpack-merge");
+const getCurrentBranch = require("git-branch");
 
-// function dynamicWebpackConfig(config = {}) {
-//   const newWebpackConfig = merge({}, webpackConfig);
-//   newWebpackConfig.entry[1] = config.entry;
-//   newWebpackConfig.output.path = config.output;
-//   // newWebpackConfig.output.filename = `js/${config.name}_[name].js`;
-//   return newWebpackConfig;
-// }
+let branchName;
 
-// console.log("-----webpackConfig-------", JSON.stringify(webpackConfig));
-const pagesOptions = {};
-console.log(">>>>>>>>");
-console.log(mGlobal.path.pages);
-
-const files = findFiles(mGlobal.path.pages, /main\.(js|ts|jsx|tsx)$/);
-console.log(files);
-
-const app = express();
-const port = 3333;
-
-app.use(express.static(path.resolve("/build")));
-
-for (const key in files) {
-  const serverItem = files[key];
-  app.get(serverItem.serverPath, (req, res) => {
-    console.log("");
-    console.log("--------------------------", serverItem.serverPath);
-
-    if (!serverItem.compiler) {
-      serverItem.title = serverItem.name;
-      const newWebpackConfig = webpackConfig(serverItem);
-      configSetFont(newWebpackConfig);
-      configSetImg(newWebpackConfig);
-      configSetCss(newWebpackConfig);
-
-      console.log("newWebpackConfig");
-      console.log(newWebpackConfig);
-      serverItem.compiler = webpack(newWebpackConfig);
-
-      app.use(
-        webpackDevMiddleware(serverItem.compiler, {
-          publicPath: newWebpackConfig.output.publicPath,
-          writeToDisk: true,
-          stats: {
-            colors: true,
-            hash: false,
-            timings: true,
-            chunks: false,
-            chunkModules: false,
-            modules: false,
-          },
-        }),
-      );
-      app.use(webpackHotMiddleware(serverItem.compiler));
-
-      serverItem.compiler.hooks.done.tap("done", () => {
-        if( serverItem.firstdone ) return
-        const indexPath = path.join(serverItem.output, "index.html");
-        console.log("-----done --sendFile---html----------------", indexPath);
-        res.sendFile(indexPath);
-        serverItem.firstdone = true
-      });
-
-    } else {
-      try {
-        const indexPath = path.join(serverItem.output, "index.html");
-        console.log("-----------html---------------", indexPath);
-        res.sendFile(indexPath);
-      } catch (error) {
-        res.writeHead(200, { "Content-Type": "application/json" });
-        res.end(JSON.stringify(serverItem));
-      }
-    }
-  });
+// 同步方式
+try {
+  branchName = getCurrentBranch.sync();
+  console.log("Current branch:", branchName);
+} catch (error) {
+  console.error("Error getting branch name:", error.message);
 }
 
-// 启动服务器
-app.listen(port, () => {
-  open(`http://localhost:${port}`);
-  console.log(`Server is running on http://localhost:${port}`);
-});
+// // function dynamicWebpackConfig(config = {}) {
+// //   const newWebpackConfig = merge({}, webpackConfig);
+// //   newWebpackConfig.entry[1] = config.entry;
+// //   newWebpackConfig.output.path = config.output;
+// //   // newWebpackConfig.output.filename = `js/${config.name}_[name].js`;
+// //   return newWebpackConfig;
+// // }
+
+// // console.log("-----webpackConfig-------", JSON.stringify(webpackConfig));
+// const pagesOptions = {};
+// console.log(">>>>>>>>");
+// console.log(mGlobal.path.pages);
+
+// const files = findFiles(mGlobal.path.pages, /main\.(js|ts|jsx|tsx)$/);
+// console.log(files);
+
+// const app = express();
+// const port = 3333;
+
+// app.use(express.static(path.resolve("/build")));
+
+// for (const key in files) {
+//   const serverItem = files[key];
+//   app.get(serverItem.serverPath, (req, res) => {
+//     console.log("");
+//     console.log("--------------------------", serverItem.serverPath);
+
+//     if (!serverItem.compiler) {
+//       serverItem.title = serverItem.name;
+//       const newWebpackConfig = webpackConfig(serverItem);
+//       configSetFont(newWebpackConfig);
+//       configSetImg(newWebpackConfig);
+//       configSetCss(newWebpackConfig);
+
+//       console.log("newWebpackConfig");
+//       console.log(newWebpackConfig);
+//       serverItem.compiler = webpack(newWebpackConfig);
+
+//       app.use(
+//         webpackDevMiddleware(serverItem.compiler, {
+//           publicPath: newWebpackConfig.output.publicPath,
+//           writeToDisk: true,
+//           stats: {
+//             colors: true,
+//             hash: false,
+//             timings: true,
+//             chunks: false,
+//             chunkModules: false,
+//             modules: false,
+//           },
+//         }),
+//       );
+//       app.use(webpackHotMiddleware(serverItem.compiler));
+
+//       serverItem.compiler.hooks.done.tap("done", () => {
+//         if( serverItem.firstdone ) return
+//         const indexPath = path.join(serverItem.output, "index.html");
+//         console.log("-----done --sendFile---html----------------", indexPath);
+//         res.sendFile(indexPath);
+//         serverItem.firstdone = true
+//       });
+
+//     } else {
+//       try {
+//         const indexPath = path.join(serverItem.output, "index.html");
+//         console.log("-----------html---------------", indexPath);
+//         res.sendFile(indexPath);
+//       } catch (error) {
+//         res.writeHead(200, { "Content-Type": "application/json" });
+//         res.end(JSON.stringify(serverItem));
+//       }
+//     }
+//   });
+// }
+
+// // 启动服务器
+// app.listen(port, () => {
+//   open(`http://localhost:${port}`);
+//   console.log(`Server is running on http://localhost:${port}`);
+// });

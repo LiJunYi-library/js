01d610f17fc52ff393047712bd91dbede290810c
LiJunYi <39401717+LiJunYi-library@users.noreply.github.com>
2025-01-10 16:16:26 +0800

    图片压缩

--- rainbow-js/package/browser/img/index.js
+++ rainbow-js/package/browser/img/index.js
@@ -1,4 +1,4 @@
-export async function loadImg(src) {
+export async function imgLoad(src) {
   if (!src) return Promise.reject();
   if (!/\.(jpg|jpeg|png|gif)/g.test(src)) return Promise.reject();
   return new Promise((resolve, reject) => {
@@ -15,24 +15,23 @@ export async function loadImg(src) {
   });
 }
 
-export function loadSSHImg(src) {
-  if (!/^(http(s?)):\/\/.*?\.(jpg|jpeg|png|gif)/g.test(src))
-    return Promise.reject();
+export function imgLoadSSH(src) {
+  if (!/^(http(s?)):\/\/.*?\.(jpg|jpeg|png|gif)/g.test(src)) return Promise.reject();
   return loadImg(src);
 }
 
-export function preLoadImg(obj) {
+export function imgPreLoad(obj) {
   JSON.stringify(obj, (key, value) => {
     loadSSHImg(value);
     return value;
   });
 }
 
-export async function preLoadImgs(...list) {
+export async function imgsPreLoad(...list) {
   return Promise.all(list.map((src) => loadImg(src)));
 }
 
-export async function requireImgFiles(imgFiles) {
+export async function imgFilesRequire(imgFiles) {
   if (!imgFiles) return Promise.reject();
   const list = imgFiles.keys().map((path) => {
     const content = imgFiles(path);
@@ -54,3 +53,51 @@ export function imgParseBlob(img, type = "image/png") {
     }, type);
   });
 }
+
+export function imgCompress(src, imageType, options = {}) {
+  const config = {
+    width: undefined,
+    height: undefined,
+    zoom: 0.5,
+    quality: 0.92,
+    ...options,
+  };
+  if (config.width) config.width = config.width * 1;
+  if (config.height) config.height = config.height * 1;
+  return new Promise((resolve) => {
+    const img = new Image();
+    img.src = src;
+    img.onload = () => {
+      const canvas = document.createElement("canvas");
+      const ctx = canvas.getContext("2d");
+      canvas.style.width = img.width;
+      canvas.style.height = img.height;
+      const { W, H } = (() => {
+        if (config.zoom) return { W: img.width * config.zoom, H: img.height * config.zoom };
+        if (config.width && !config.height) {
+          return { W: config.width, H: (config.width / img.width) * img.height };
+        }
+        if (config.height && !config.width) {
+          return { W: (config.height / img.height) * img.width, H: config.height };
+        }
+        return { W: config.width, H: config.height };
+      })();
+      canvas.width = W;
+      canvas.height = H;
+      ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+      canvas.toBlob(
+        (blob) => {
+          resolve({ blob, base64: canvas.toDataURL(imageType, config.quality), img });
+        },
+        imageType,
+        config.quality,
+      );
+    };
+  });
+}
+
+export const loadImg = imgLoad;
+export const loadSSHImg = imgLoadSSH;
+export const preLoadImg = imgPreLoad;
+export const preLoadImgs = imgsPreLoad;
+export const requireImgFiles = imgFilesRequire;

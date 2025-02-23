88d323d88bf048c41bcbb237aa2f6e9be7161c26
LiJunYi <39401717+LiJunYi-library@users.noreply.github.com>
2025-02-23 18:53:07 +0800

    Update index.js

--- rainbow-js/package/promise/index.js
+++ rainbow-js/package/promise/index.js
@@ -38,3 +38,7 @@ export function useQueue(props = {}) {
 export function setTimeoutPromise(time = 0, resolveValue = time) {
   return new Promise((resolve) => setTimeout(() => resolve(resolveValue), time));
 }
+
+export function requestAnimationFramePromise(value) {
+  return new Promise((resolve) => requestAnimationFrame(() => resolve(value)));
+}

631faffaf0be6d045c4100737ea62cef7322b463
LiJunYi <39401717+LiJunYi-library@users.noreply.github.com>
2025-03-25 13:09:23 +0800

    flex auto 布局

--- vue/v-doc/src/views/layout/flex/index.vue
+++ vue/v-doc/src/views/layout/flex/index.vue
@@ -1,26 +1,14 @@
 <template>
   <div class="r-absolute-demo">
-
       <r-flex >
         <r-flex >1111</r-flex>
-        <r-flex >2222</r-flex>
+        <r-flex auto>2222</r-flex>
         <r-flex >3333</r-flex>
       </r-flex>
-
   </div>
 </template>
 <script setup>
-const list = [
-  'center',
-  'top-left',
-  'top-center',
-  'top-right',
-  'right-center',
-  'right-bottom',
-  'bottom-center',
-  'bottom-left',
-  'left-center',
-]
+
 </script>
 
 <style scoped lang="scss">

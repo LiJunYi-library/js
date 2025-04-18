d85ac6cd390a001614359a4f9b7e4a5ac2b40b06
LiJunYi <39401717+LiJunYi-library@users.noreply.github.com>
2025-04-18 15:33:07 +0800

    Update index.d.ts

---
+++ rainbow-js/package/fetch/index.d.ts
@@ -0,0 +1,55 @@
+type ANY = any;
+type FunReturnVal = () => ANY;
+
+export declare interface FetchConfig<T = ANY, D = ANY> extends RequestInit {
+  url?: string;
+  urlParams?: ANY;
+  body?: ANY;
+  baseUrl?: string = "";
+  time?: number = 30000;
+  isDownloadFile?: boolean = false;
+  fileName?: string;
+  isFormBody?: boolean;
+  isJsonBody?: boolean;
+  loading?: boolean = false;
+  begin?: boolean = false;
+  error?: boolean = false;
+  data?: ANY;
+  errorData?: ANY;
+
+  formatterResponse?: (res: Response, config: this) => Promise<ANY>;
+  formatterData?: (data: D, rest: T, res: Response, config: this) => ANY;
+  formatterFile?: (res: Response, config: this) => Promise<File>;
+  formatterFileName?: (res: Response, config: this) => Promise<string>;
+  formatterBody?: (config: this) => Promise<undefined | object>;
+  parseBody?: (config: this) => Promise<string>;
+  formatterUrlParams?: (config: this) => Promise<undefined | object>;
+  downloadFile?: (res: Response, config: this, file: File) => void;
+  interceptResponseSuccess?: (res: Response, rest: T, config: this) => Promise<D>;
+  interceptResponseError?: (error: ANY, config: this, resolve: (value: any) => void, reject: (value: any) => void) => void;
+  interceptRequest?: (config: this) => void;
+  onAbort?: (error: ANY, config: this, resolve: (value: any) => void, reject: (value: any) => void) => void;
+  onTimeoutAbort?: (error: ANY, config: this, resolve: (value: any) => void, reject: (value: any) => void) => void;
+  onLoading?: (error: ANY, config: this, resolve: (value: any) => void, reject: (value: any) => void) => void;
+  onRequest?: (config: this) => ANY;
+  onResponse?: (config: this) => ANY;
+}
+
+
+export class Fetch<T = any, D = any> {
+  loading: Boolean;
+  begin: Boolean;
+  error: Boolean;
+  data: D;
+  errorData: any;
+  config: FetchConfig<T, D>;
+  send: (config: FetchConfig<T, D>) => Promise<D>;
+  nextSend: (config: FetchConfig<T, D>) => Promise<D>;
+  awaitSend: (config: FetchConfig<T, D>) => Promise<D>;
+  beginSend: (config: FetchConfig<T, D>) => Promise<D>;
+  nextBeginSend: (config: FetchConfig<T, D>) => Promise<D>;
+  awaitBeginSend: (config: FetchConfig<T, D>) => Promise<D>;
+  abortPrve: () => void;
+  abort: () => void;
+  constructor(props?: FetchConfig<T, D> = {})
+}

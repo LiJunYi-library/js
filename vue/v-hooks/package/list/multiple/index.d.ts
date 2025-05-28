6e42bf8db7af35288427faebbba301827c6fa72b
LiJunYi <39401717+LiJunYi-library@users.noreply.github.com>
2025-05-28 19:19:46 +0800

    去除ListMultiple

--- vue/v-hooks/package/list/multiple/index.d.ts
+++ vue/v-hooks/package/list/multiple/index.d.ts
@@ -1,3 +1,2 @@
 import { ListMultiple } from "@rainbow_ljy/rainbow-js";
-export declare class VListMultiple extends ListMultiple {}
 export declare function useListMultiple(props: any): any;
--- vue/v-hooks/package/list/multiple/index.js
+++ vue/v-hooks/package/list/multiple/index.js
@@ -1,48 +1,5 @@
-import { ListMultiple, listMultiple } from "@rainbow_ljy/rainbow-js";
+import { listMultiple } from "@rainbow_ljy/rainbow-js";
 import { ref } from "vue";
-export class VListMultiple extends ListMultiple {
-  get list() {
-    return this.states.list.value;
-  }
-  set list(v) {
-    this.states.list.value = v;
-  }
-  get value() {
-    return this.states.value.value;
-  }
-  set value(v) {
-    this.states.value.value = v;
-  }
-  get label() {
-    return this.states.label.value;
-  }
-  set label(v) {
-    this.states.label.value = v;
-  }
-  get select() {
-    return this.states.select.value;
-  }
-  set select(v) {
-    this.states.select.value = v;
-  }
-  get index() {
-    return this.states.index.value;
-  }
-  set index(v) {
-    this.states.index.value = v;
-  }
-
-  setStates() {
-    return {
-      list: ref([]),
-      value: ref(),
-      label: ref(),
-      select: ref(),
-      index: ref(),
-    };
-  }
-}
-
 export function useListMultiple(props = {}) {
   return listMultiple({
     selectRef: ref,

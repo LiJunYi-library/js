70decaf103a1cf9f6f063d475be2984fb6050384
LiJunYi <39401717+LiJunYi-library@users.noreply.github.com>
2025-08-06 18:02:14 +0800

    objectParseUri

--- rainbow-js/package/object/index.js
+++ rainbow-js/package/object/index.js
@@ -15,7 +15,7 @@ export function objectFilter(object = {}, regExp, isDelete) {
 
 export function objectIsEmpty(object) {
   if (!object) return false;
-  return !Object.keys(object).length;
+  return Boolean(Object.keys(object).length);
 }
 
 export function objectEqualitys(o1 = {}, o2 = {}) {
@@ -26,25 +26,16 @@ export function objectEqualitys(o1 = {}, o2 = {}) {
 }
 
 export function objectParseUri(url = "", object) {
-  if (!object) return "";
   if (typeof object !== "object") return object;
-  if (!Object.keys(object).length) return "";
-  let str = "";
-  for (const key in object) {
-    if (Object.hasOwnProperty.call(object, key)) {
-      let element = object[key];
-      if (typeof element === "object") {
-        if (element instanceof Array) element = element.toString();
-        else element = JSON.stringify(element);
-      }
-      if (element === undefined) continue;
-      if (element + "" === "NaN") element = null;
-      str += `${key}=${element}&`;
-    }
-  }
-  str = str.slice(0, -1);
-  if (url && url.includes("?")) return `&${str}`;
-  return `?${str}`;
+  if (!objectIsEmpty(object)) return "";
+  const params = new URLSearchParams();
+  objectForIn(object, (item, key) => {
+    if (item === undefined) return;
+    if (typeof item === "object" && !(item instanceof Array)) item = JSON.stringify(item);
+    params.set(key, item);
+  });
+  if (url && url.includes("?")) return `&${params.toString()}`;
+  return `?${params.toString()}`;
 }
 
 export function objectParseFormData(object, bool) {
